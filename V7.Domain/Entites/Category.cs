0e377b6c98d42dd6af3f0767b0951f554e4d5455
Abdelkariem Hany <74042738+abdelkareem25@users.noreply.github.com>
2026-04-28 21:37:59 +0300

    update Category and Product Entity

--- V7.Domain/Entites/Category.cs
+++ V7.Domain/Entites/Category.cs
@@ -3,8 +3,8 @@ namespace V7.Domain.Entites
 {
     public class Category :BaseEntity
     {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Description { get; set; } 
         public ICollection<Product> Product { get; set; } = new List<Product>();
     }
 }
--- V7.Domain/Entites/Product.cs
+++ V7.Domain/Entites/Product.cs
@@ -3,10 +3,10 @@ namespace V7.Domain.Entites
 {
     public class Product : BaseEntity
     {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
-        public string PictureUrl { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } 
 
         public int CategoryId { get; set; } // FK
         public Category? Category { get; set; }
